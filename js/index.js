56e19b94589641d054429afe00630c7c4d4b5f44
Javie Isaiah L. Mostoles <javiemostoles1@gmail.com>
2025-04-16 23:11:17 +0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 const title = document.querySelector('.title')
-const text = 'I click mo yung open dali hehehe'.split('')
+const text = 'I have something to tell you, click the open'.split('')
 for (let index = 0; index < text.length; index++) {
   if (text[index] !== ' ') {
     title.innerHTML += `<span>${text[index]}<span/>`
